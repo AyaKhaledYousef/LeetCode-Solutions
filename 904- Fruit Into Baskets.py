ca890d9a7bde21736d9c6e0782792b80ad93a4d4
AyaKhaledYousef <aya.k.yousef@gmail.com>
2023-02-07 09:47:50 +0200

    Day 5

---
+++ 904- Fruit Into Baskets.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+fruits=[1,2,5]
+
+def totalFruit (fruits):
+    count = defaultdict(int) # defaultdict to store the frequency of the elements in the subarray
+    i, j = 0, 0 # variables i and j are used to keep track of the subarray boundaries
+
+    for j in range(len(fruits)):
+        
+        count[fruits[j]] += 1 # increase the count of the current fruit
+        # print(count)
+        
+        if len(count) > 2:
+            
+            count[fruits[i]] -= 1 # reduce the count of the first fruit
+            # print(count)
+            
+            if count[fruits[i]] == 0:
+                
+                del count[fruits[i]] # remove the first fruit from the defaultdict if its count becomes 0
+                # print(count)
+                
+            i += 1 # move the start of the subarray to the right
+    return j-i +1
+
+print("result",totalFruit(fruits))
